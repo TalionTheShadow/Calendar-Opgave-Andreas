0f7ad3c85bf036c153e0befcae74c71494e57eff
Andreas Carl <andreascarl@hotmail.dk>
2023-09-20 10:52:07 +0200

    Listen virker nu, jeg mangler bare at få den til at udskrive i dansk.

--- Calendar Opgave Andreas/Pages/Index.razor.cs
+++ Calendar Opgave Andreas/Pages/Index.razor.cs
@@ -24,61 +24,66 @@ namespace Calendar_Opgave_Andreas.Pages
         static int year = 2022;
         static string BaseApiUrl = $"https://holidayapi.com/v1/holidays?key={apiKey}&country={country}&year={year}";
 
-      
-        
+
+
         public class Dag
         {
             public DateTime? Dato { get; set; }
             public string? Navn { get; set; }
         }
 
-        public List<Dag>? helligdage { get; set; }
+        public List<Dag>? helligdage { get; set; } = new List<Dag>();
 
         protected override async Task OnInitializedAsync()
         {
             Console.WriteLine("Before");
             helligdage = await GetHelligdag();
             Console.WriteLine("After");
-            
-        }
 
+        }
         async Task<List<Dag>> GetHelligdag()
         {
+
             using (HttpClient client = new HttpClient())
             {
-                Console.WriteLine("FFFFF");
                 HttpResponseMessage svar = await client.GetAsync(BaseApiUrl);
                 if (svar.IsSuccessStatusCode)
                 {
+                    Console.WriteLine("Succes");
                     string json = await svar.Content.ReadAsStringAsync();
                     JObject jsonObject = JObject.Parse(json);
                     JArray? helligdageArray = jsonObject["helligdage"] as JArray;
-                    if (helligdageArray != null)
+                    JArray? holidaysArray = jsonObject["holidays"] as JArray;
+                    if (holidaysArray != null)
                     {
-                        var helligdageList = new List<Dag>();
-                        foreach (JObject helligdageObject in helligdageArray)
+                        var holidayList = new List<Dag>();
+                        foreach (JObject holidayObject in holidaysArray)
                         {
-                            string? navn = helligdageObject["navn"]?.ToString();
-                            DateTime? dato = helligdageObject["dato"]?.ToObject<DateTime?>();
+                            string? name = holidayObject["name"]?.ToString();
+                            DateTime? date = holidayObject["date"]?.ToObject<DateTime?>();
                             Dag holiday = new Dag()
                             {
-                                Navn = navn,
-                                Dato = dato
+                                Navn = name,
+                                Dato = date
                             };
-                            helligdageList.Add(holiday);
+                            holidayList.Add(holiday);
                         }
-                        return helligdageList;
+                        return holidayList;
+
+
                     }
                     else
                     {
-                        Console.WriteLine("Fejl");
                         return new List<Dag>();
+
                     }
-
                 }
-            }
+                else
+                {
                     return new List<Dag>();
+                }
 
+            }
         }
     }
 }
